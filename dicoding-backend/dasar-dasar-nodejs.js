16942442ba6b534fee21942b5d4b0b59c963df1c
amien1410 <68118454+amien1410@users.noreply.github.com>
2022-06-19 14:32:35 +0800

    Update and rename tutorial-1.js to dasar-dasar-nodejs.js

---
+++ dicoding-backend/dasar-dasar-nodejs.js
@@ -0,0 +1,78 @@
+// Dasar-dasar Nodejs
+
+// Events
+const { EventEmitter } = require('events');
+ 
+const myEventEmitter = new EventEmitter();
+ 
+const makeCoffee = (name) => {
+    console.log(`Kopi ${name} telah dibuat!`);
+};
+ 
+const makeBill = (price) => {
+    console.log(`Bill sebesar ${price} telah dibuat!`);
+}
+ 
+const onCoffeeOrderedListener = ({ name, price }) => {
+    makeCoffee(name);
+    makeBill(price);
+}
+ 
+myEventEmitter.on('coffee-order', onCoffeeOrderedListener);
+ 
+myEventEmitter.emit('coffee-order', { name: 'Tubruk', price: 15000 });
+ 
+/**
+ * output:
+ * Kopi Tubruk telah dibuat!
+ * Bill sebesar 15000 telah dibuat!
+ */
+
+// Membaca File / FileSystem
+
+// 1- Asynchronous 
+const fs = require('fs');
+ 
+const fileReadCallback = (error, data) => {
+    if(error) {
+        console.log('Gagal membaca berkas');
+        return;
+    }
+    console.log(data);
+};
+ 
+fs.readFile('todo.txt', 'UTF-8', fileReadCallback);
+
+// 2 - Synchronous
+const fs = require('fs');
+ 
+const data = fs.readFileSync('todo.txt', 'UTF-8');
+console.log(data);
+
+// 3 - Read File Stream
+const fs = require('fs');
+ 
+const readableStream = fs.createReadStream('./article.txt', {
+    highWaterMark: 10
+});
+ 
+readableStream.on('readable', () => {
+    try {
+        process.stdout.write(`[${readableStream.read()}]`);
+    } catch(error) {
+        // catch the error when the chunk cannot be read.
+    }
+});
+ 
+readableStream.on('end', () => {
+    console.log('Done');
+});
+
+// 4 - Write File Stream
+const fs = require('fs');
+ 
+const writableStream = fs.createWriteStream('output.txt');
+ 
+writableStream.write('Ini merupakan teks baris pertama!\n');
+writableStream.write('Ini merupakan teks baris kedua!\n');
+writableStream.end('Akhir dari writable stream!');
